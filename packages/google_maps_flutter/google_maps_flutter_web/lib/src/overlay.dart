4c2f877e1dd6c7149ae99140fe108bc44124fe3f
Eli Geller <eli@reportallusa.com>
2023-01-05 10:25:38 -0500

    oops

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/overlay.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/overlay.dart
@@ -1,90 +1,52 @@
 part of google_maps_flutter_web;
 
-/// This class manages all the [TileOverlayController]s associated to a [GoogleMapController].
-class TileOverlaysController {
-  /// The [gmaps.GMap] instance that this controller operates on.
-  late gmaps.GMap googleMap;
+/// This wraps a [TileOverlay] in a [gmaps.MapType].
+class TileOverlayController {
+  /// For consistency with Android, this is not configurable.
+  // TODO(AsturaPhoenix): Verify consistency with iOS.
+  static const int logicalTileSize = 256;
 
-  final Map<TileOverlayId, TileOverlayController> _tileOverlays =
-      <TileOverlayId, TileOverlayController>{};
-  final List<TileOverlayController> _visibleTileOverlays =
-      <TileOverlayController>[];
-
-  void _insertZSorted(TileOverlayController tileOverlayController) {
-    final int index = _visibleTileOverlays.lowerBoundBy<num>(
-        tileOverlayController,
-        (TileOverlayController c) => c.tileOverlay.zIndex);
-
-    googleMap.overlayMapTypes!.insertAt(index, tileOverlayController.gmMapType);
-    _visibleTileOverlays.insert(index, tileOverlayController);
+  /// Updates the [gmaps.MapType] and cached properties with an updated
+  /// [TileOverlay].
+  void update(TileOverlay tileOverlay) {
+    _tileOverlay = tileOverlay;
+    _gmMapType = gmaps.MapType()
+      ..tileSize = gmaps.Size(logicalTileSize, logicalTileSize)
+      ..getTile = _getTile;
   }
 
-  void _removeZSorted(TileOverlayController tileOverlayController) {
-    final int index = _visibleTileOverlays.indexOf(tileOverlayController);
-    googleMap.overlayMapTypes!.removeAt(index);
-    _visibleTileOverlays.removeAt(index);
-  }
-
-  /// Adds new [TileOverlay]s to this controller.
-  ///
-  /// Wraps the [TileOverlay]s in corresponding [TileOverlayController]s.
-  void addTileOverlays(Set<TileOverlay> tileOverlays) {
-    for (final TileOverlay tileOverlay in tileOverlays) {
-      final TileOverlayController controller = TileOverlayController()
-        ..update(tileOverlay);
-      _tileOverlays[tileOverlay.tileOverlayId] = controller;
-
-      if (tileOverlay.visible) {
-        _insertZSorted(controller);
-      }
-    }
-  }
-
-  /// Updates [TileOverlay]s with new options.
-  void changeTileOverlays(Set<TileOverlay> tileOverlays) {
-    for (final TileOverlay tileOverlay in tileOverlays) {
-      final TileOverlayController controller =
-          _tileOverlays[tileOverlay.tileOverlayId]!;
-
-      final bool wasVisible = controller.tileOverlay.visible;
-      final bool zIndexChanged =
-          tileOverlay.zIndex != controller.tileOverlay.zIndex;
-      if (wasVisible && !tileOverlay.visible || zIndexChanged) {
-        _removeZSorted(controller);
-      }
-
-      controller.update(tileOverlay);
-
-      if (tileOverlay.visible) {
-        if (!wasVisible || zIndexChanged) {
-          _insertZSorted(controller);
-        } else {
-          final int i = _visibleTileOverlays.indexOf(controller);
-          googleMap.overlayMapTypes!.setAt(i, controller.gmMapType);
-        }
-      }
-    }
-  }
-
-  /// Removes the tile overlays associated with the given [TileOverlayId]s.
-  void removeTileOverlays(Set<TileOverlayId> tileOverlayIds) {
-    for (final TileOverlayId id in tileOverlayIds) {
-      final TileOverlayController controller = _tileOverlays.remove(id)!;
-      if (controller.tileOverlay.visible) {
-        _removeZSorted(controller);
-      }
-    }
-  }
-
-  /// Invalidates the tile overlay associated with the given [TileOverlayId].
-  void clearTileCache(TileOverlayId tileOverlayId) {
-    final TileOverlayController? controller = _tileOverlays[tileOverlayId];
-    if (controller == null || !controller.tileOverlay.visible) {
-      return;
+  HtmlElement? _getTile(
+      gmaps.Point? tileCoord, num? zoom, Document? ownerDocument) {
+    if (_tileOverlay.tileProvider == null) {
+      return null;
     }
 
-    final int i = _visibleTileOverlays.indexOf(controller);
-    googleMap.overlayMapTypes!.setAt(i, controller.gmMapType);
-    // It's that simple; this causes the map to reload the overlay.
+    final ImageElement img =
+        ownerDocument!.createElement('img') as ImageElement;
+    img.width = img.height = logicalTileSize;
+    img.hidden = true;
+    _tileOverlay.tileProvider!
+        .getTile(tileCoord!.x!.toInt(), tileCoord.y!.toInt(), zoom?.toInt())
+        .then((Tile tile) async {
+      if (tile.data == null) {
+        return;
+      }
+
+      // Using img lets us take advantage of native decoding.
+      final String src = Url.createObjectUrl(Blob(<dynamic>[tile.data]));
+      img.src = src;
+      await img.decode();
+      img.hidden = false;
+      Url.revokeObjectUrl(src);
+    });
+    return img;
   }
+
+  /// The [gmaps.MapType] produced by this controller.
+  gmaps.MapType get gmMapType => _gmMapType;
+  late gmaps.MapType _gmMapType;
+
+  /// The [TileOverlay] providing data for this controller.
+  TileOverlay get tileOverlay => _tileOverlay;
+  late TileOverlay _tileOverlay;
 }
